9f8c0251dde4baf89f57ff5969a370a3249f6437
User <polic.21@mail.ru>
2023-10-05 17:32:30 +0300

    first

---
+++ main.cpp
@@ -0,0 +1,183 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+using namespace sf;
+
+int N = 20, M = 20;
+int ts = 25;
+
+int q = 0;
+
+int dir = 2, num = 4;
+
+bool game = true;
+
+
+struct Snake {
+    int x, y;
+} s[600];
+
+
+struct Fruct
+{
+    int x, y;
+
+} f;
+
+
+void Tick() {
+
+    for (int i = num; i > 0; i--) {
+        s[i].x = s[i - 1].x;
+        s[i].y = s[i - 1].y;
+    }
+
+    if (dir == 0)
+        s[0].y += 1; //вниз
+    if (dir == 1)
+        s[0].x -= 1; //влево
+    if (dir == 2)
+        s[0].x += 1; //право
+    if (dir == 3)
+        s[0].y -= 1; //вверх
+
+    //Здесь отвечает, если змейка уходит через стенку экрана
+    //то она возращается на через другую
+    if (s[0].x > N)
+        s[0].x = 0;
+    if (s[0].x < 0)
+        s[0].x = N;
+    if (s[0].y > M)
+        s[0].y = 0;
+    if (s[0].y < 0)
+        s[0].y = M;
+
+    //Если змейка поедает яблоко
+    if ((s[0].x == f.x) && (s[0].y == f.y)) {
+        num++; //Змейка растёт
+        q = q + 10;
+        f.x = rand() % N;
+        f.y = rand() % M;
+    }
+
+    //Здесь проигрышь
+    for (int i = 1; i < num; i++)
+        if ((s[0].x == s[i].x) && (s[0].y == s[i].y))
+            game = false;
+}
+
+int main()
+{
+    srand(time(0));
+
+    RenderWindow window(VideoMode(ts * N, ts * M), "Snake!");
+
+
+
+    Texture t;
+    t.loadFromFile("C:/Users/User/CLionProjects/untitled/block.png");
+    Sprite tiles(t);
+
+    //Ставим картинку змейки
+    Texture sn;
+    sn.loadFromFile("C:/Users/User/CLionProjects/untitled/snake.png");
+    Sprite snake(sn);
+
+    //Ставим картинку яблоко
+    Texture ap;
+    ap.loadFromFile("C:/Users/User/CLionProjects/untitled/apple.png");
+    Sprite apple(ap);
+
+    //Ставим надпись gameover
+    Texture go;
+    go.loadFromFile("C:/Users/User/CLionProjects/untitled/gameover.png");
+    Sprite gameover(go);
+    gameover.setPosition(0, 0);
+
+    f.x = 10;
+    f.y = 10;
+    //первичные координаты яблоки
+
+    Clock clock; //переменная clock нужна для игрового времени
+    float timer = 0, delay = 0.3;
+    //timer и delay отвечают за скорость змейки
+    //timer отвечает, через какое время змейка двигается
+    //delay - за задержку
+
+    while (window.isOpen())
+    {
+        //Игровое время
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        //Управление змейкой через клавиатуру
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+            dir = 1;
+        if (Keyboard::isKeyPressed(Keyboard::A))
+            dir = 1;
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+            dir = 2;
+        if (Keyboard::isKeyPressed(Keyboard::D))
+            dir = 2;
+        if (Keyboard::isKeyPressed(Keyboard::W))
+            dir = 3;
+        if (Keyboard::isKeyPressed(Keyboard::Up))
+            dir = 3;
+        if (Keyboard::isKeyPressed(Keyboard::S))
+            dir = 0;
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            dir = 0;
+
+        //Здесь отвечает за работу игры
+        if (timer > delay && game) {
+            timer = 0; //Возвращаем время
+            Tick(); //Вызываем работу игры
+        }
+
+        window.clear();
+
+        //Для прорисовки всего графичесского окна
+        //Пишем цикл for
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++) {
+                tiles.setPosition(i * ts, j * ts); //Позиция
+                window.draw(tiles); //Рисуем
+            }
+
+        //Рисуем змейку
+        for (int i = 0; i < num; i++) {
+            if (i != 0) //Рисуем тело змейки
+                snake.setTextureRect(IntRect(0, 0, ts, ts));
+            else //Рисуем голову змейки
+                snake.setTextureRect(IntRect(dir * ts, ts, ts, ts));
+
+            // Во время проигрыша голова изменяется
+            if (!game && i == 1)
+                snake.setTextureRect(IntRect(dir * ts, ts * 2, ts, ts));
+
+            snake.setPosition(s[i].x * ts, s[i].y * ts); //Задаём позицию
+            //для плиток змейки
+            window.draw(snake);
+        }
+
+        apple.setPosition(f.x * ts, f.y * ts); //Позиция яблоки
+        window.draw(apple); //Рисуем яблоко
+
+        //Если мы проиграли, то будеть выдовать надпись gameover
+        if (!game)
+            window.draw(gameover);
+
+        window.display();
+        ;
+    }
+
+    return 0;
+}
